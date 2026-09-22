a2a649a0c15c7616b98338fd2dd36909d28d9d8c
GUNDLURU RAJASEKHAR <gundlururajashekar@gmail.com>
2026-09-22 10:07:33 +0530

    Update prisma import path in auth.controller.js

--- auth.controller.js
+++ auth.controller.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const prisma = require('../config/db');
+const prisma = require('./db');
 
 function signToken(user) {
   return jwt.sign(
